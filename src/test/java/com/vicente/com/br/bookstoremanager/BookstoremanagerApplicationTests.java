699fb4df4505e25ab8e25c2eda7fbf988c2dbf5f
Vicente Nobre <vpnobre@gmail.com>
2021-06-14 09:28:06 -0300

    primeiro commit projeto criado

---
+++ src/test/java/com/vicente/com/br/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicente.com.br.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
