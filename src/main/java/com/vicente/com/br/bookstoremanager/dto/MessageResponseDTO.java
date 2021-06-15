50a14c201d115000cabe899878b88ba1de7cf37f
Vicente Nobre <vpnobre@gmail.com>
2021-06-14 22:22:00 -0300

    Criação do serviço de criar livros

---
+++ src/main/java/com/vicente/com/br/bookstoremanager/dto/MessageResponseDTO.java
@@ -0,0 +1,13 @@
+package com.vicente.com.br.bookstoremanager.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MessageResponseDTO {
+
+	private String message;
+	
+	
+}
